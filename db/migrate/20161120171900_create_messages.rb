b5656162a65e161d7b03b77eea0d716a3f9c26e3
norika69 <non69.0u0.s2@gmail.com>
2016-11-21 15:16:21 +0900

    change column constraint of message table
    messageテーブルのカラム作成
    カラム,型,制約
    body,text,null_false
    image,string,
    group_id,integer,foreign_key
    user_id,integer,foreign_key

--- db/migrate/20161120171900_create_messages.rb
+++ db/migrate/20161120171900_create_messages.rb
@@ -1,10 +1,10 @@
 class CreateMessages < ActiveRecord::Migration[5.0]
   def change
     create_table :messages do |t|
-      t.text  :body, :null => false
+      t.text  :body, null: false
       t.string  :image
-      t.integer  :group_id
-      t.integer  :user_id
+      t.integer  :group_id, foreign_key: true
+      t.integer  :user_id, foreign_key: true
       t.timestamps
     end
   end
