fb516766b337498eef7d06ed977cfb27f305bddf
norika69 <non69.0u0.s2@gmail.com>
2016-11-23 19:22:48 +0900

    edit groups_controller.rb
    グループ入力時にgroupモデルにカラムが保存されるように編集

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,12 +1,18 @@
 class GroupsController < ApplicationController
 
   def new
-
+    @group = Group.new
   end
 
   def create
-
+    @groups = Group.create(create_params)
+    redirect_to controller: :messages, action: :index
   end
-  
+
+
+  private
+  def create_params
+    params.require(:group).permit(:group_name)
+  end
 end
 
