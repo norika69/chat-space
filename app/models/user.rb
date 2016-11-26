23f54a9ce01756b818fe555a951d3855d8979e4e
norika69 <non69.0u0.s2@gmail.com>
2016-11-26 13:56:22 +0900

    change user model
    add has_many :groups

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  has_many :groups
+  #一対多の時 currentuserがグループ作成した時（管理側）
+  # has_many :groups, through: :user_group
+  #多対多の時 中間テーブルにidを保存するため（参加者側）
+  has_many :user_group
+  has_many :messages
   validates :email, presence: true
 end
