30a6c1b764a572ac7feba810bc984739f7f07dd9
norika69 <non69.0u0.s2@gmail.com>
2016-11-27 15:41:53 +0900

    edit user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :groups
-  #一対多の時 currentuserがグループ作成した時（管理側）
-  # has_many :groups, through: :user_group
-  #多対多の時 中間テーブルにidを保存するため（参加者側）
-  has_many :user_group
   has_many :messages
+  has_many :user_groups
+  has_many :groups, through: :user_groups
   validates :email, presence: true
 end
