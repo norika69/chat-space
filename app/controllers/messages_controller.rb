828be1ddffdab67a2a78725bd14cd9c1f9e967b4
norika69 <non69.0u0.s2@gmail.com>
2016-11-28 11:46:25 +0900

    add create action to messages_controller
    グループテーブルからparamsを探してくる
    @messageにgroupテーブルのidに関連付けしてmessage_paramsのストロングパラメータをかけて入力されたmessageのカラ
    ムを作る
    バリテーション確認
    もし＠messageに保存されたらshowにリデイレクト
    違ったらshowに飛んでエラー文を出す
    （.full_messages[0]はエラー文を全部表示し、[0]は括弧をなくす）

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,34 @@
 class MessagesController < ApplicationController
-  def index
-    @group = Group.find(current_user)
+
+  def create
+    group = Group.find(params[:id])
+    @message = group.messages.build(message_params)
+    @message.valid?
+    if @message.save
+       redirect_to group_path(params[:id])
+    else
+       redirect_to group_path(params[:id]),alert: @message.errors.full_messages[0]
+    end
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+private
+  def message_params
+    params.require(:message).permit(:user_id, :body, :image)
+  end
+
+  def set_message
+    @group = Group.find(params[:id])
   end
 end
