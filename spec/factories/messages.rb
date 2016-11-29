f3f1b1a9855d24e36e130294307bcb7fb02a82b3
norika69 <non69.0u0.s2@gmail.com>
2016-11-29 17:29:54 +0900

    create factories/messages.rb
    factory_girlにデータ登録

---
+++ spec/factories/messages.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+
+  factory :message do
+    body                  "らんらんるー"
+    image                 "rururu-"
+    group_id              "1"
+    user_id               "5"
+  end
+
+end
