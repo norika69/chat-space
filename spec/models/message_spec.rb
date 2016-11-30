5190087a35dec116592f73ea8dd37c449660a23a
norika69 <non69.0u0.s2@gmail.com>
2016-11-30 13:15:54 +0900

    edit message_spec.rb
    add context do

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,14 +1,11 @@
 require 'rails_helper'
 	describe Message do
-	  describe '#create' do
+	  context '#create' do
 	    it 'is invalid without a body' do
 	    	message = build(:message, body:"")
 	    	message.valid?
 	    	expect(message.errors[:body]).to include("を入力してください")
-	    end
-	  end
-
-    describe '#create' do
+  	   end
       it "is valid" do
         message = build(:message)
         expect(message).to be_valid
