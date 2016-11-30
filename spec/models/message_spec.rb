2e8947511b35ee8dadef660d6fca6bb6655ca7ce
norika69 <non69.0u0.s2@gmail.com>
2016-11-30 12:42:22 +0900

    edit message_spec.rb

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,17 +1,18 @@
 require 'rails_helper'
+	describe Message do
+	  describe '#create' do
+	    it 'is invalid without a body' do
+	    	message = build(:message, body:"")
+	    	message.valid?
+	    	expect(message.errors[:body]).to include("を入力してください")
+	    end
+	  end
 
-describe Message do
-  describe '#create' do
-    it "is invalid without a body" do
-    message = build(:message, body: "")
-    message.valid?
-    expect(message.errors[:body]).to include("を入力してください")
+    describe '#create' do
+      it "is valid" do
+        message = build(:message)
+        expect(message).to be_valid
+      end
     end
   end
-  describe '#create' do
-    it "is valid" do
-    message = build(:message)
-    expect(message).to be_valid
-    end
-  end
-end
+
