4da496d1acecd0d599984c195386b3d227aede2d
norika69 <non69.0u0.s2@gmail.com>
2016-12-02 17:20:08 +0900

    edit factories/messages.rb
    不要な記述の削除

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -5,9 +5,7 @@ FactoryGirl.define do
   factory :message do
     body              {Faker::Pokemon.name}
     image             { fixture_file_upload Rails.root.join('spec', 'fixtures', 'dummy.jpg'), 'image/jpeg' }
-    user_id            1
-    group_id           1
-
+    
     after(:create) do |message|
       message.groups << create(:group)
     end
