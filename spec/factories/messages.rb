82d9b0705027d4cdb24ab128ecf208aaa38ec337
norika69 <non69.0u0.s2@gmail.com>
2016-12-02 14:36:10 +0900

    create dummy info
    テスト用ダミー情報作成
    @user.rb
    @group.rb
    @message.rb
    @user_groups

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -5,6 +5,13 @@ FactoryGirl.define do
   factory :message do
     body              {Faker::Pokemon.name}
     image             { fixture_file_upload Rails.root.join('spec', 'fixtures', 'dummy.jpg'), 'image/jpeg' }
+    user_id            1
+    group_id           1
+
+    after(:create) do |message|
+      message.groups << create(:group)
+    end
   end
+
 
 end
