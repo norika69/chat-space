a559605cfde6f6dcd225a87b94cd4f5ab588df12
norika69 <non69.0u0.s2@gmail.com>
2016-12-14 12:45:41 +0900

    change migration files

--- db/migrate/20161121104657_add_column_to_messages.rb
+++
@@ -1,6 +0,0 @@
-class AddColumnToMessages < ActiveRecord::Migration[5.0]
-  def change
-    add_reference :messages, :group, foreign_key: true
-    add_reference :messages, :user, foreign_key: true
-  end
-end
